06cb70283a00a7b9210fd906e8c65ac0396cb1eb
liqing <liqing@511314.fun>
2020-06-28 13:51:57 +0800

    预加载图片

---
+++ web/src/config/imgList.js
@@ -0,0 +1,4 @@
+export default [
+    require('@/assets/qw.jpg'),
+    require('@/assets/west.jpg'),
+];
--- web/src/main.js
+++ web/src/main.js
@@ -3,11 +3,23 @@ import App from './App.vue'
 import router from './router'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import { imgsPreloader } from './config/imgPreloader.js';
+import imgPreloaderList from './config/imgList.js';
 
 Vue.config.productionTip = false
 Vue.use(Vant);
+//预加载图片
+(async () => {
+  await imgsPreloader(imgPreloaderList);
+  //关闭加载弹框
+  document.querySelector('.loading').style.display = 'none';
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app');
+})();
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
